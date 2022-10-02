f1dd3646c0cf865ab8dd677ca541bf2522de1747
NickKennedy00 <justnick098@gmail.com>
2022-10-02 15:05:30 +0700

    update proses

---
+++ Test Suites/cobaKatJen.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>cobaKatJen</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>bddc3e5d-862c-41bf-a95d-12ea4be6b15b</testSuiteGuid>
+   <testCaseLink>
+      <guid>a9469cc2-5cca-4686-a55b-2169087180bb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/cobaRecord</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
